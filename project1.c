64fb016f2897fb6fb0d757dfc54cc97c42e2b153
Maverick-P <48081807+Maverick-P@users.noreply.github.com>
2019-04-04 18:56:13 +1100

    Update project1.c

--- project1.c
+++ project1.c
@@ -7,29 +7,41 @@ int main()
 {
 int n = 0;
 int f = 0;
-while(n > -1)
+char rotmesg[100];
+char submesg[100];
+while(n < 10)
 {
 printf("Please choose your type of service:\nRotation encryption: 1 \nRotation decryption: 2\nSubstitution encryption: 3\nSubstitution decryption: 4\nEXIT: 5\n", &n);
 scanf("%d", &n);
     switch (n){
         case 1:
-            printf("Please write the message you should like to encrypt:\n");
-            printf("would you like to exit or try another?\nYES: 1\nNO: 2\n");
+            printf("Please write the message you should like to encrypt:\n", &rotmesg);
+            scanf("%s", &rotmesg);
+            printf("%s\n", rotmesg);
+    break;
         case 2:
-            printf("Please write the message you should like to decrypt:\n");  
+            printf("Please write the message you should like to decrypt:\n", &rotmesg);
+            scanf("%s", &rotmesg);
+            printf("%s\n", rotmesg);
     break;
         case 3:
-            printf("Please write the message you should like to encrypt:\n");
+            printf("Please write the message you should like to encrypt:\n", &submesg);
+            scanf("%s", &submesg);
+            printf("%s\n", submesg);
     break;
         case 4:
-            printf("Please write the message you should like to decrypt:\n"); 
+            printf("Please write the message you should like to decrypt:\n", &submesg); 
+            scanf("%s", &submesg);
+            printf("%s\n", &submesg);
     break;
         case 5:
             exit(0);
         default:
         printf("please choose a number between 1 and 5:\n");
-   
 }
+ printf("would you like to exit?\nYES: 1\nNO: 2\n", &n);
+ scanf("%d", &n);
+ if (n==1){exit(0);}
 }
  return 0;
 }
