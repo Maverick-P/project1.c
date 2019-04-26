aee9594ff25992ab9cbcf9fca277134059ebffdd
Maverick-P <48081807+Maverick-P@users.noreply.github.com>
2019-04-26 12:38:59 +1000

    Update project1.c
    this code is to do substitution cipher encrypt and decrypt

--- project1.c
+++ project1.c
@@ -1,22 +1,13 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<string.h>
 
-char CaesarCipher(char[], int i);
-
+void UCASE(char []);
 int main()
 {
-int n = 0;
-int f = 0;
-int j;
-char code[1000], ln;
-while(n < 10)
-{
-printf("Please choose your type of service:\nRotation encryption: 1 \nRotation decryption: 2\nSubstitution encryption: 3\nSubstitution decryption: 4\nEXIT: 5\n", &n);
-scanf("%d", &n);
-    switch (n){
-        case 1:
-            printf("Please write the message you should like to encrypt:\n", &rotmesg);
+	char code[1000], ln;
+	int j;
+	
+	printf("Enter a message to encrypt: ");
 	gets(code);
 	UCASE(code);
 	for(j = 0; code[j] != '\0'; ++j){
@@ -33,7 +24,26 @@ scanf("%d", &n);
 		}
 	}
 	
-	printf("Encrypted message: %s", (code));
+	printf("Encrypted message: %s\n", (code));
+	
+	printf("Enter a message to decrypt: ");
+	gets(code);
+	UCASE(code);
+	for(j = 0; code[j] != '\0'; ++j){
+		ln = code[j];
+		
+		if(ln >= 'A' && ln <= 'Z'){
+			ln = ln + 16;
+			
+			if(ln > 'Z'){
+				ln = ln - 'Z' + 'A' - 1;
+			}
+			
+			code[j] = ln;
+		}
+	}
+	
+	printf("Encrypted message: %s\n", (code));
 	
 	return 0;
 }
@@ -47,43 +57,4 @@ void UCASE(char s[]) {
       }
       c++;
    }
-}
-            printf("would you like to decrypt this code?\nYES:1\nNO:2\n", &f);
-            scanf("%d", f);
-            printf("%d", f);
-            if(f==2){exit(0);}
-        case 2:
-            printf("Please write the message you should like to decrypt:\n", &rotmesg);
-            scanf("\n");
-            scanf ("%[^\n]%*c", &rotmesg);
-            printf("%s\n", rotmesg);
-    break;
-        case 3:
-            printf("Please write the message you should like to encrypt:\n", &submesg);
-            scanf("\n");
-             scanf ("%[^\n]%*c", &submesg);
-            printf("%s\n", submesg);
-    break;
-        case 4:
-            printf("Please write the message you should like to decrypt:\n", &submesg);
-            scanf("\n");
-             scanf ("%[^\n]%*c", &submesg);
-            printf("%s\n", &submesg);
-    break;
-        case 5:
-            exit(0);
-        default:
-        printf("please choose a number between 1 and 5:\n");
-}
- printf("would you like to exit?\nYES: 1\nNO: 2\n", &n);
- scanf("%d", &n);
- if (n==1){exit(0);}
-}
- return 0;
-}
-
-char CaesarCipher(char[], int i)
-{
-    i=3;
-    return i;
 }
